ee9039e203e7f5ff2f5200e7a3c17472bf93c21f
Takudzwa Gwindingwi <takueg@gmail.com>
2021-11-02 20:44:05 +0200

    functions to pattern match lists

--- lib/pattern_matching/lists.ex
+++ lib/pattern_matching/lists.ex
@@ -11,29 +11,29 @@ defmodule PatternMatching.Lists do
   Fix or complete the code to make the tests pass.
   """
 
-  def is_empty?(list) do
-    [] == list
-  end
+  def is_empty?([]), do: true
+  def is_empty?(_other), do: false
 
-  def has_1_item?(list) do
-    length(list) == 1
-  end
+  def has_1_item?([_item]), do: true
+  def has_1_item?(_other), do: false
 
-  def at_least_one?(list) do
-    length(list) >= 1
-  end
+  def at_least_one?([_head|_tail]), do: true
+  def at_least_one?(_other), do: false
 
   def return_first_item([head | _tail]), do: head
   def return_first_item([]), do: :error
 
   def starts_with_1?([1 | _tail]), do: true
+  def starts_with_1?(_other), do: false
 
-  def sum_pair(_list) do
+  def sum_pair([a, b]), do: a+b
+  def sum_pair(_other), do: :error
 
+  def sum_first_2([a | [b | tail]]) do
+    [a+b | tail]
   end
 
-  def sum_first_2(_list) do
-
-  end
+  def sum_first_2([item]), do: [item]
+  def sum_first_2([]), do: []
 
 end
